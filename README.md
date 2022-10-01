4162f898268e5ee490033ad0e651ff9521be09aa
Arturs Smirnovs <yam.aka.as@gmail.com>
2022-10-01 20:16:29 +0300

    Store admin password hashed in DB

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Hey everyone I made this entire blog system with php, It took me a lot of hardwork to make.
 If you have any improvements then just make it.
+
+#### DEFAULT ADMIN LOGIN
+
+Username: programerd
+
+Password: 284101
--- admin/config.php
+++ admin/config.php
@@ -4,6 +4,8 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'db_blog');
 
+define('PASSWORD_SALT', '$2a$09$anefkbglvssgnggwarghhp$');
+
 $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if(!$conn){
